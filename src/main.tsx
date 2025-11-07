9e78c93cd10962e747b5434e06626f7c4258bf19
Nicolò Carello <39197035+carellonicolo@users.noreply.github.com>
2025-11-07 11:28:48 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+document.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
